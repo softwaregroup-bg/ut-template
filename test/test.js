3725c709918ac7731015e939383192a124114ec7
stamen.peev <stamen.peev@softwaregroup-bg.com>
2015-04-02 19:14:47 +0300

    unescape 'less than' in sql

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 if (fs.existsSync('./test.xml.marko.js')) fs.unlinkSync('./test.xml.marko.js');
 if (fs.existsSync('./test.sql.marko.js')) fs.unlinkSync('./test.sql.marko.js');
 if (fs.existsSync('./test.json.marko.js')) fs.unlinkSync('./test.json.marko.js');
+if (fs.existsSync('./unesc.sql.marko.js')) fs.unlinkSync('./unesc.sql.marko.js');
 
 var t = require('ut-template');
 t.init({
@@ -33,21 +34,28 @@ t.init({
 var xml = t.load(require.resolve('./test.xml.marko'));
 var sql = t.load(require.resolve('./test.sql.marko'));
 var json = t.load(require.resolve('./test.json.marko'));
+var unesc = t.load(require.resolve('./unesc.sql.marko'));
 
 xml.render({username:'admin'}).then(function(res){
-    console.log('XML=',res);
+    console.log('\n\n--------------\nXML=',res);
 }).catch(function(err){
-    console.log('XML error=',err);
+    console.log('\n\n--------------\nXML error=',err);
 })
 
 sql.render({username:'admin'}).then(function(res){
-    console.log('SQL=',res);
+    console.log('\n\n--------------\nSQL=',res);
 }).catch(function(err){
-    console.log('SQL error=',err);
+    console.log('\n\n--------------\nSQL error=',err);
 })
 
 json.render({username:'admin'}).then(function(res){
-    console.log('JSON=',res);
+    console.log('\n\n--------------\nJSON=',res);
 }).catch(function(err){
-    console.log('JSON error=',err);
+    console.log('\n\n--------------\nJSON error=',err);
+})
+
+unesc.render({username:'admin'}).then(function(res){
+    console.log('\n\n--------------\nUNESCAPE SQL=',res);
+}).catch(function(err){
+    console.log('\n\n--------------\nUNESCAPE SQL error=',err);
 })
